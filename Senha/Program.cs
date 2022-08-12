18561c02faa48079ce2bff9cae07c7685b5e213a
alber <alber@ALBERTO_BARBOSA>
2022-08-12 11:01:28 -0300

    Adicionar arquivos de projeto.

---
+++ Senha/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Senha;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int A, B;
+
+        Console.WriteLine("Vamos cadastrar uma senha agora");
+        Console.Write("Digite uma senha apenas com numeros por favor: ");
+        A = int.Parse(Console.ReadLine());
+        Console.WriteLine("Agora para acessar, digite sua senha");
+        B = int.Parse(Console.ReadLine());
+
+        while(A != B)
+        {
+            Console.WriteLine("ACESSO NEGADO! DIGITE NOVAMENTE A SENHA");
+            B = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("ACESSO PERMITIDO");
+        Console.WriteLine("Seja bem vindo");
+
+    }
+}
